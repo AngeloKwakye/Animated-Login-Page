ce8dabc29efc8ab2c0855436c3de773a66340dac
D'Angelo <dangelokwakye@gmail.com>
2024-01-24 05:37:41 +0000

    login and sign up animation

---
+++ script.js
@@ -0,0 +1,13 @@
+const registerBtn = document.getElementById('register')
+const container = document.getElementById('container')
+const login = document.getElementById('login')
+
+
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+login.addEventListener('click', () => {
+    container.classList.remove('active');
+});
